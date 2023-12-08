54958b6ab8ff45265ffd16cca457c659d1613159
taebrizini <104359216+ATTAEBRIZINI000@users.noreply.github.com>
2023-12-08 21:02:12 +0100

    update :  initial project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
+import React from 'react';
+import Header from './header';
+import { ChakraProvider ,CSSReset, Container, extendTheme, Box } from '@chakra-ui/react';
+import Sidebar from './sidebar';
 
-import { Button } from '@chakra-ui/react'
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        fontFamily: 'body',
+      },
+    },
+  },                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+});
 
 function App() {
- 
-
   return (
-  <>
-<Button colorScheme='blue'>Button</Button>
-  </>
+    <ChakraProvider theme={theme}>
+      {/* Header */}
+       <Header />
+      <CSSReset />
+      <Container display="flex" maxWidth="100vw" height="100vh" padding="0">
+        {/* Sidebar */}
+        <Sidebar />
+
+        {/* Main Content */}
+        <Box flex="1" padding="4">
+          {/* Main Content */}
+        </Box>
+      </Container>
+
+      
      
-  )
+    </ChakraProvider>
+  );
 }
 
-export default App
+export default App;
